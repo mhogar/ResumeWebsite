b3505da6e89fad1e55c5acf0ace700f1677594f7
Ryan Stamp <mhogar98@gmail.com>
2019-01-12 23:13:08 -0500

    Added languages section to home page

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-$(document).ready(function() {
-	//$('#menu-bar').sticky();
-});
-
 var home_page_template = `
 	<div>
 		<div class="ui blue inverted segment">
@@ -23,7 +19,44 @@ var home_page_template = `
 			So much creativity goes into making these virtual worlds, and as stated above, that's how I express myself. 
 			I hope to someday make games that people will play and enjoy.
 		</p>
+		<div class="ui blue inverted segment">
+			<h4>Languages</h4>
+		</div>
+		<table class="ui center aligned very basic table">
+			<thead>
+				<tr>
+					<th>Language</th>
+					<th>Experience</th>
+					<th>Preference</th>
+				</tr>
+			</thead>
+			<tbody>
+				<LanguageTableRow name="C" rating="5" preference="2" />
+				<LanguageTableRow name="C#" rating="4" preference="5" />
+				<LanguageTableRow name="C++" rating="3" preference="4" />
+				<LanguageTableRow name="Java" rating="4" preference="3" />
+				<LanguageTableRow name="Ruby" rating="2" preference="4" />
+				<LanguageTableRow name="Ruby on Rails" rating="4" preference="5" />
+				<LanguageTableRow name="HTML" rating="5" preference="3" />
+				<LanguageTableRow name="CSS" rating="3" preference="2" />
+				<LanguageTableRow name="JavaScript" rating="5" preference="4" />
+				<LanguageTableRow name="PHP" rating="4" preference="4" />
+				<LanguageTableRow name="SQL" rating="4" preference="4" />
+			</tbody>
+		</table>
 	</div>
+`;
+
+var language_table_row_template =  `
+	<tr>
+		<td>{{name}}</td>
+		<td>
+			<div class="ui star rating" v-bind:data-rating="rating" data-max-rating="5"></div>
+		</td>
+		<td>
+			<div class="ui heart rating" v-bind:data-rating="preference" data-max-rating="5"></div>
+		</td>
+	</tr>
 `;
 
 var project_page_template = `
@@ -131,11 +164,17 @@ var coop_page_template = `
 var vue = new Vue({
 	el: '#page',
 	data: {
-		page: 'projects'
+		page: 'home'
 	},
 	components: {
 		'home': {
-			template: home_page_template
+			template: home_page_template,
+			components: {
+				'LanguageTableRow': {
+					props: ['name', 'rating', 'preference'],
+					template: language_table_row_template
+				}
+			}
 		},
 		'projects': {
 			template: project_page_template
@@ -151,7 +190,15 @@ var vue = new Vue({
 			//unselect all menu items then select the clicked one
 			['home', 'projects', 'coop'].forEach((item) => $('#' + item + '-item').removeClass('active'));
 			$('#' + page + '-item').addClass('active');
+		},
+		onPageSwitch: function() {
+			if (this.page == 'home') {
+				$('.ui.rating').rating('disable');
+			}
 		}
+	},
+	mounted: function() {
+		this.onPageSwitch();
 	}
 });
 
